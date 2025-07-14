5af0a3ff3791125c8db7e7edb63916fc99bbd05b
madathala-niharika <niharikamadathala@gmail.com>
2025-07-14 18:39:05 +0530

    static change in the  settings.py

--- travelplan/travelplan/settings.py
+++ travelplan/travelplan/settings.py
@@ -120,6 +120,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "travelplan" / "static",
 ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
