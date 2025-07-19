275b87a3226ec4ce577c9fce20e7adb51ada8150
madathala-niharika <niharikamadathala@gmail.com>
2025-07-19 20:41:23 +0530

    creater an app and models and DBmigrations

--- travelplan/exploremore/models.py
+++ travelplan/exploremore/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class ExploreMore(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='places_images/')
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
